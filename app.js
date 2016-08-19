430f26eb0820b3f9d17bb0bc4941226be00471b8
ShekharReddy4 <reddyshekhar619@gmail.com>
2016-08-19 13:15:25 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine','ejs');
+
+app.get('/', function (req, res) {
+    res.render('index',{
+        isAuthenticated: false,
+        user: req.user
+    })
+});
+
+app.get('/login', function(req, res){
+  res.render('login');
+});
+
+app.post('/login', function(req, res) {
+  res.send("hello");
+});
+
+var port = process.env.PORT || 8000;
+
+app.listen(port, function(){
+    console.log('http://127.0.0.1:'+port+'/');
+});
